27a16ebc43a82a20e857fa2ea319c7b6c3ea9201
gregoire <gregoire_brebner@hotmail.com>
2024-05-30 21:50:55 +0200

    Make output more readable and greatly simplify code by removing files from output

---
+++ FolderNode.py
@@ -0,0 +1,18 @@
+import humanize
+
+class FolderNode(object):        
+    def __init__(self, name:str):
+        self.name = name
+        self.size = 0
+    
+    def getLabel(self) -> str:
+        return self.name + ' - ' + humanize.naturalsize(self.size)
+    
+    def isDir(self) -> bool:
+        return True
+    
+    def addToSize(self, size: int) -> None:
+        self.size += size
+        
+
+        
--- analyser.py
+++ analyser.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 import argparse
-from treelib import Tree, Node
-from FSNode import *
+from treelib import Tree
+from FolderNode import *
 
 # TODO: 
 # - Add depth limit on graph
 # - Find way of diplaying graph or better saving mechanism
 # - Make larger values of each level more obvious
+# - Comments
 
 def main():
     args = initArgParser()
@@ -21,10 +22,9 @@ def main():
         for file in files:
             fullPath = root / file
             fileSize = fullPath.stat().st_size
-            addFileNode(tree, fullPath, FileNode(file, fileSize))  
             updateSizes(tree, fullPath, fileSize)
             
-    setDirLabels(tree)    
+    setLabels(tree)    
     tree.save2file("graph.txt")
 
 def initArgParser() -> argparse.Namespace:
@@ -32,24 +32,26 @@ def initArgParser() -> argparse.Namespace:
     parser.add_argument("root", help="The path to the root directory whose contents should be analysed")
     parser.add_argument("-d", "--depth", type=int, help="How deep should the tree be displayed")
     return parser.parse_args()
-
-
-def addFileNode(tree: Tree, path: Path, node: FileNode) -> None:
-    tree.create_node(node.getLabel(),str(path),data=node, parent=str(path.parent))
     
 def addFolderNode(tree: Tree, path: Path, node: FolderNode) -> None:
     tree.create_node(node.name,str(path),data=node, parent=str(path.parent))
     
 def updateSizes(tree: Tree, path: Path, size:int) -> None:
-    node = tree.parent(str(path))
+    try:
+        node = tree.get_node(str(path.parent))
+    except Exception as e:
+        print("Size update failed")
+        print(e)
+        return
+        
     if(node == None):
         return
     
     node.data.addToSize(size) 
     updateSizes(tree, path.parent, size)
     
-def setDirLabels(tree:Tree) -> None:
-    for nodeStr in tree.expand_tree(filter = lambda x: x.data.isDir()):
+def setLabels(tree:Tree) -> None:
+    for nodeStr in tree.expand_tree():
         node = tree.get_node(nodeStr)
         node.tag = node.data.getLabel()
 

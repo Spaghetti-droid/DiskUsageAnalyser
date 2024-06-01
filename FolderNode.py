7d788d3d087f3cc15838828cd55bab62b6790595
gregoire <gregoire_brebner@hotmail.com>
2024-06-01 13:07:08 +0200

    Add more comments

--- FolderNode.py
+++ FolderNode.py
@@ -1,18 +1,27 @@
 import humanize
 
-class FolderNode(object):        
+class FolderNode(object):  
+    """ Holds individual folder data for the tree
+    """      
     def __init__(self, name:str):
         self.name = name
         self.size = 0
         self.toRemove = False
     
-    def getLabel(self) -> str:
+    def generateLabel(self) -> str:
+        """Use name and size to create a formatted label
+
+        Returns:
+            str: Label displaying name and size
+        """
         return self.name + ' - ' + humanize.naturalsize(self.size)
     
-    def isDir(self) -> bool:
-        return True
-    
     def addToSize(self, size: int) -> None:
+        """ Update self.size by adding size to it
+
+        Args:
+            size (int):
+        """
         self.size += size
         
 
